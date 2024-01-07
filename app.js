9eddee03a54bfc8e367b553abfe507efa832f266
kfiatekk <115893974+kfiatekk@users.noreply.github.com>
2024-01-08 00:15:03 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const THUMBNAILS = document.querySelectorAll(".thumbnail img")
+const POPUP = document.querySelector(".popup")
+const POPUP_CLOSE = document.querySelector(".popup_close")
+const POPUP_IMG = document.querySelector(".popup__img")
+
+THUMBNAILS.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", (e) => {
+        POPUP.classList.remove("hidden");
+        POPUP_IMG.src = e.target.src;
+    });
+});
+
+POPUP_CLOSE.addEventListener("click", () => {
+    POPUP.classList.add("hidden")
+}
+)
+
